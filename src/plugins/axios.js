27632774b109d48173bc55ca310323dd3a244ff4
Gantira Suriadinata <gantira11@gmail.com>
2022-08-17 12:15:22 +0700

    test cors

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -6,7 +6,6 @@ import axios from "axios";
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 // axios.defaults.headers.post['Content-Type'] = 'multipart/form-data; boundary=<calculated when request is sent>';
 
 let config = {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+import { defineConfig } from "@vue/cli-service";
+export default defineConfig({
   transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: 'https://dolphin-app-o4fcf.ondigitalocean.app'
+  },
   pwa: {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
